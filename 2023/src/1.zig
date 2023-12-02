dfccf5a816e032b1594a55cc6b1ae036ea8657dd
Alessio Paccoia <alessiopcc91@gmail.com>
2023-12-02 20:29:10 +0100

    add solution day 1 part 2

--- 2023/src/1.zig
+++ 2023/src/1.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
+    try part_1();
+    try part_2();
+}
+
+fn part_1() !void {
     const file = try std.fs.cwd().openFile("../input/1.txt", .{});
     defer file.close();
 
@@ -37,5 +42,48 @@ pub fn main() !void {
         accumulator += (first.* - ansi_base) * 10 + (second.* - ansi_base);
     }
 
-    std.log.info("result part 1 -> {d}\n", .{accumulator});
+    std.log.info("result 1 -> {d}", .{accumulator});
+}
+
+fn part_2() !void {
+    const file = try std.fs.cwd().openFile("../input/1.txt", .{});
+    defer file.close();
+
+    var io_reader = std.io.bufferedReader(file.reader());
+    const stream = io_reader.reader();
+
+    var accumulator: usize = 0;
+
+    var values = [18][]const u8{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    var l: [1024]u8 = undefined;
+    while (try stream.readUntilDelimiterOrEof(&l, '\n')) |line| {
+        var first: usize = undefined;
+        var first_pos: usize = line.len;
+        var second: usize = undefined;
+        var second_pos: usize = 0;
+
+        for (0.., values) |i, value| {
+            var lpos = std.mem.indexOf(u8, line, value);
+            var rpos = std.mem.lastIndexOf(u8, line, value);
+
+            if (lpos) |pos| {
+                if (pos <= first_pos) {
+                    first_pos = pos;
+                    first = (i % 9) + 1;
+                }
+            }
+
+            if (rpos) |pos| {
+                if (pos >= second_pos) {
+                    second_pos = pos;
+                    second = (i % 9) + 1;
+                }
+            }
+        }
+
+        accumulator += first * 10 + second;
+    }
+
+    std.log.info("result 2 -> {d}", .{accumulator});
 }
