2df9aaec12ebb1ec0212db05b80752431affb437
Alessio Paccoia <alessiopcc91@gmail.com>
2023-12-02 21:56:52 +0100

    add solution day 2 part 2

--- 2023/src/2.zig
+++ 2023/src/2.zig
@@ -14,14 +14,14 @@ fn part_1() !void {
     var io_reader = std.io.bufferedReader(file.reader());
     const stream = io_reader.reader();
 
-    var accumulator: usize = 0;
-
     const allocator = std.heap.page_allocator;
     var bag = std.StringHashMap(usize).init(allocator);
     defer bag.deinit();
     try bag.put("red", 12);
     try bag.put("green", 13);
     try bag.put("blue", 14);
+
+    var accumulator: usize = 0;
 
     var l: [1024]u8 = undefined;
     while (try stream.readUntilDelimiterOrEof(&l, '\n')) |line| {
@@ -66,11 +66,39 @@ fn part_2() !void {
     var io_reader = std.io.bufferedReader(file.reader());
     const stream = io_reader.reader();
 
+    const allocator = std.heap.page_allocator;
+
     var accumulator: usize = 0;
 
     var l: [1024]u8 = undefined;
     while (try stream.readUntilDelimiterOrEof(&l, '\n')) |line| {
-        _ = line;
+        var line_iter = std.mem.split(u8, line, ": ");
+        _ = line_iter.first();
+
+        var bag = std.StringHashMap(usize).init(allocator);
+        defer bag.deinit();
+
+        var turn_iter = std.mem.split(u8, line_iter.rest(), "; ");
+        while (turn_iter.next()) |turn| {
+            var cube_iter = std.mem.split(u8, turn, ", ");
+            while (cube_iter.next()) |cube| {
+                var value_iter = std.mem.split(u8, cube, " ");
+                var num = try std.fmt.parseInt(usize, value_iter.first(), 10);
+                var color = value_iter.rest();
+
+                if (!bag.contains(color) or num > bag.get(color).?) {
+                    try bag.put(color, num);
+                }
+            }
+        }
+
+        var bag_accumulator: usize = 1;
+        var bag_iter = bag.valueIterator();
+        while (bag_iter.next()) |value| {
+            bag_accumulator *= value.*;
+        }
+
+        accumulator += bag_accumulator;
     }
 
     std.log.info("result 2 -> {d}", .{accumulator});
