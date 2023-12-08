75cfa9ec1452fc0184b3e5ade59b4c1a9adcb5ef
Alessio Paccoia <alessiopcc91@gmail.com>
2023-12-08 22:36:29 +0100

    add solution day 4 part 2

--- 2023/src/4.zig
+++ 2023/src/4.zig
@@ -73,11 +73,81 @@ fn part_2() !void {
     var io_reader = std.io.bufferedReader(file.reader());
     const stream = io_reader.reader();
 
+    var allocator = std.heap.page_allocator;
+    var map = std.AutoHashMap(usize, usize).init(allocator);
+    defer map.deinit();
+    var total_cards: usize = 0;
+
     var accumulator: usize = 0;
 
     var l: [1024]u8 = undefined;
     while (try stream.readUntilDelimiterOrEof(&l, '\n')) |line| {
-        _ = line;
+        total_cards += 1;
+        var line_iter = std.mem.split(u8, line, ": ");
+
+        var card_biter = std.mem.splitBackwards(u8, line_iter.first(), " ");
+        var card_id = try std.fmt.parseInt(usize, card_biter.first(), 10);
+
+        var card_iter = std.mem.split(u8, line_iter.rest(), "|");
+
+        var winning_list = std.ArrayList(usize).init(allocator);
+        defer winning_list.deinit();
+        try parse_insert_sorted(&winning_list, card_iter.first());
+
+        var number_list = std.ArrayList(usize).init(allocator);
+        defer number_list.deinit();
+        try parse_insert_sorted(&number_list, card_iter.rest());
+
+        var winning_count: usize = 0;
+        var end = false;
+
+        var i: usize = 0;
+        var winning = winning_list.items[i];
+
+        for (number_list.items) |number| {
+            while (number > winning) {
+                i += 1;
+
+                if (i == winning_list.items.len) {
+                    end = true;
+                    break;
+                }
+
+                winning = winning_list.items[i];
+            }
+
+            if (end)
+                break;
+
+            if (number == winning) {
+                winning_count += 1;
+            }
+        }
+
+        if (winning_count > 0) {
+            var j: usize = 1;
+            var multiplier: usize = 1;
+
+            if (map.contains(card_id))
+                multiplier += map.get(card_id).?;
+
+            while (j <= winning_count) : (j += 1) {
+                if (!map.contains(card_id + j)) {
+                    _ = try map.put(card_id + j, 0);
+                }
+                try map.put(card_id + j, map.get(card_id + j).? + multiplier);
+            }
+        }
+    }
+
+    accumulator += total_cards;
+
+    var i: usize = 0;
+    while (i <= total_cards) : (i += 1) {
+        var value = map.get(i);
+        if (value) |v| {
+            accumulator += v;
+        }
     }
 
     std.log.info("result 2 -> {d}", .{accumulator});
